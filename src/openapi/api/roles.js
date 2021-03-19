dd23f70ed5b6944cc844bfffb66b87b3c039a680
lautaro valle lopez <lautarovallelopez@gmail.com>
2021-03-19 18:45:24 -0300

    feat: add delete method for roles

--- src/openapi/api/roles.js
+++ src/openapi/api/roles.js
@@ -79,5 +79,126 @@ module.exports = {
                 }
             }
         }
+    },
+    '/api/roles/{id}': {
+        get: {
+            security: [
+                {bearerAuth: []}
+            ],
+            parameters: [
+                {
+                    in: 'path',
+                    name: 'id',
+                    required: true,
+                    schema: {
+                        type: 'string',
+                        maxLength: 50
+                    },
+                    description: 'Id Role to find'
+                }
+            ],
+            responses: {
+                200: {
+                    description: 'Success',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {
+                                    role: { $ref: '#/components/schemas/Role'}
+                                },
+                                example: {
+                                    role: {
+                                        id: 'SUPERVISOR',
+                                        description: 'Supervisor description',
+                                        observation: 'Supervisor observation',
+                                        domain: 'Supervisor domain'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        },
+        put: {
+            security: [
+                {bearerAuth: []}
+            ],
+            parameters: [
+                {
+                    in: 'path',
+                    name: 'id',
+                    required: true,
+                    schema: {
+                        type: 'string',
+                        maxLength: 50
+                    },
+                    description: 'Role to update'
+                }
+            ],
+            requestBody: {
+                required: true,
+                content: { 'application/json': {schema: {$ref: '#/components/schemas/Role'}}}
+            },
+            responses: {
+                200: {
+                    description: 'Success',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {
+                                    role: { $ref: '#/components/schemas/Role'}
+                                },
+                                example: {
+                                    role: {
+                                        id: 'SUPERVISOR',
+                                        description: 'Supervisor description',
+                                        observation: 'Supervisor observation',
+                                        domain: 'Supervisor domain'
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        },
+        delete: {
+            security: [
+                {bearerAuth: []}
+            ],
+            parameters: [
+                {
+                    in: 'path',
+                    name: 'id',
+                    required: true,
+                    schema: {
+                        type: 'string',
+                        maxLength: 50
+                    },
+                    description: 'Id of role to delete'
+                }
+            ],
+            responses: {
+                200: {
+                    description: 'Success',
+                    content: {'application/json': { schema: { $ref: '#/components/schemas/Success'}}}
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        }
     }
 };
--- src/routes/api/roles.js
+++ src/routes/api/roles.js
@@ -4,5 +4,9 @@ module.exports = router => {
     router.route('/')
         .get(RoleController.fetch)
         .post(RoleController.create);
+    router.route('/:id')
+        .put(RoleController.update)
+        .get(RoleController.find)
+        .delete(RoleController.delete);
     return router;
 };
