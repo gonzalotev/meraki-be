51c6a33f59f6889dde71c47dd156e8b97ed98aca
ncosentino <ncosentino@indec.gob.ar>
2021-04-05 14:22:22 -0300

    fix: all be

--- src/routes/api/operatives.js
+++ src/routes/api/operatives.js
@@ -4,9 +4,6 @@ module.exports = router => {
     router.route('/')
         .get(OperativesController.fetch)
         .post(OperativesController.create)
-        .put(OperativesController.update)
-        // .delete(OperativesController.delete);
-    // router.route('/:DESCRIPCION/:ID_OPERATIVO/:ID_FUENTE')
-    //     .get(OperativesController.fetchOne);
+        .put(OperativesController.update);
     return router;
 };
