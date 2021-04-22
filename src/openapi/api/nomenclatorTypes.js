637879dc4581a68a91c806f11e3bfb7bb85e4684
ncosentino <ncosentino@indec.gob.ar>
2021-04-22 19:53:42 -0300

    fix(): nomenclatorTypes api

--- src/openapi/api/nomenclatorTypes.js
+++ src/openapi/api/nomenclatorTypes.js
@@ -2,6 +2,7 @@ module.exports = {
     '/api/nomenclatorTypes': {
         get: {
             security: [{bearerAuth: []}],
+            tags: ['Types'],
             responses: {
                 200: {
                     description: 'Success',
@@ -10,28 +11,23 @@ module.exports = {
                             schema: {
                                 type: 'object',
                                 properties: {
-                                    nomenclatorTypes: {
+                                    nomenclators: {
                                         type: 'array',
-                                        items: { $ref: '#/components/schemas/NomenclatorTypes'}
-                                    }
-                                },
-                                example: {
-                                    nomenclatorType: [
-                                        {
-                                            id: 'ACT',
-                                            description: 'ACT description',
-                                            supervised: false,
-                                            observation: 'ACT observation',
-                                            domain: 'ACT domain'
-                                        },
-                                        {
-                                            id: 'CCO',
-                                            description: 'CCO description',
-                                            supervised: false,
-                                            observation: 'CCO observation',
-                                            domain: 'CCO domain'
+                                        items: {
+                                            type: 'object',
+                                            properties: {
+                                                id: {type: 'string'},
+                                                description: {type: 'string'},
+                                                observation: {type: 'string'},
+                                                domain: {type: 'string'},
+                                                approved: {type: 'boolean'},
+                                                createdAt: {type: 'string'},
+                                                userCreator: {type: 'string'},
+                                                userDeleted: {type: 'string'},
+                                                deletedAt: {type: 'string'}
+                                            }
                                         }
-                                    ]
+                                    }
                                 }
                             }
                         }
@@ -45,26 +41,50 @@ module.exports = {
         },
         post: {
             security: [{bearerAuth: []}],
+            tags: ['Types'],
             requestBody: {
+                description: 'The new  type of nomenclator to create',
                 required: true,
-                content: { 'application/json': {schema: {$ref: '#/components/schemas/NomenclatorTypes'}}}
+                content: {
+                    'application/json': {
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                description: {type: 'string'},
+                                observation: {type: 'string'},
+                                domain: {type: 'string'},
+                                approved: {type: 'boolean'},
+                                createdAt: {type: 'string'},
+                                userCreator: {type: 'string'},
+                                userDeleted: {type: 'string'},
+                                deletedAt: {type: 'string'}
+                            }
+                        }
+                    }
+                }
             },
             responses: {
-                200: {
-                    description: 'Success',
+                201: {
+                    description: 'ok',
                     content: {
                         'application/json': {
                             schema: {
                                 type: 'object',
                                 properties: {
-                                    nomenclatorType: { $ref: '#/components/schemas/NomenclatorTypes'}
-                                },
-                                example: {
-                                    role: {
-                                        id: 'ASD',
-                                        description: 'ASD description',
-                                        observation: 'ASD observation',
-                                        domain: 'ASD domain'
+                                    success: {type: 'boolean'},
+                                    nomenclator: {
+                                        type: 'object',
+                                        properties: {
+                                            id: {type: 'string'},
+                                            description: {type: 'string'},
+                                            observation: {type: 'string'},
+                                            domain: {type: 'string'},
+                                            approved: {type: 'boolean'},
+                                            createdAt: {type: 'string'},
+                                            userCreator: {type: 'string'},
+                                            userDeleted: {type: 'string'},
+                                            deletedAt: {type: 'string'}
+                                        }
                                     }
                                 }
                             }
@@ -79,86 +99,62 @@ module.exports = {
         }
     },
     '/api/nomenclatorTypes/{id}': {
-        get: {
-            security: [
-                {bearerAuth: []}
-            ],
-            parameters: [
-                {
-                    in: 'path',
-                    name: 'id',
-                    required: true,
-                    schema: {
-                        type: 'string',
-                        maxLength: 10
-                    },
-                    description: 'Id nomenclatorType to find'
-                }
-            ],
-            responses: {
-                200: {
-                    description: 'Success',
-                    content: {
-                        'application/json': {
-                            schema: {
-                                type: 'object',
-                                properties: {
-                                    role: { $ref: '#/components/schemas/NomenclatorTypes'}
-                                },
-                                example: {
-                                    role: {
-                                        id: 'QWE',
-                                        description: 'QWE description',
-                                        observation: 'QWE observation',
-                                        domain: 'QWE domain'
-                                    }
-                                }
-                            }
-                        }
-                    }
-                },
-                default: {
-                    description: 'Error',
-                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
-                }
-            }
-        },
         put: {
-            security: [
-                {bearerAuth: []}
-            ],
+            security: [{bearerAuth: []}],
+            tags: ['Types'],
             parameters: [
                 {
                     in: 'path',
                     name: 'id',
                     required: true,
-                    schema: {
-                        type: 'string',
-                        maxLength: 50
-                    },
-                    description: 'Nomenclator Type to update'
+                    schema: {type: 'integer'},
+                    description: 'User id of assignment'
                 }
             ],
             requestBody: {
+                description: 'The new  type of nomenclator to create',
                 required: true,
-                content: { 'application/json': {schema: {$ref: '#/components/schemas/NomenclatorTypes'}}}
+                content: {
+                    'application/json': {
+                        schema: {
+                            type: 'object',
+                            properties: {
+                                id: {type: 'string'},
+                                description: {type: 'string'},
+                                observation: {type: 'string'},
+                                domain: {type: 'string'},
+                                approved: {type: 'boolean'},
+                                createdAt: {type: 'string'},
+                                userCreator: {type: 'string'},
+                                userDeleted: {type: 'string'},
+                                deletedAt: {type: 'string'}
+                            }
+                        }
+                    }
+                }
             },
             responses: {
                 200: {
-                    description: 'Success',
+                    description: 'ok',
                     content: {
                         'application/json': {
                             schema: {
                                 type: 'object',
                                 properties: {
-                                    nomenclatorTypes: { $ref: '#/components/schemas/NomenclatorTypes'}
-                                },
-                                example: {
-                                    nomenclatorType: {
-                                        id: 'QWE',
-                                        description: 'QWE description',
-                                        observation: 'QWE observation',
-                                        domain: 'QWE domain'
+                                    success: {type: 'boolean'},
+                                    nomenclator: {
+                                        type: 'object',
+                                        properties: {
+                                            id: {type: 'string'},
+                                            description: {type: 'string'},
+                                            observation: {type: 'string'},
+                                            domain: {type: 'string'},
+                                            approved: {type: 'boolean'},
+                                            createdAt: {type: 'string'},
+                                            userCreator: {type: 'string'},
+                                            userDeleted: {type: 'string'},
+                                            deletedAt: {type: 'string'}
+                                        }
                                     }
                                 }
                             }
@@ -172,25 +168,66 @@ module.exports = {
             }
         },
         delete: {
-            security: [
-                {bearerAuth: []}
-            ],
+            security: [{bearerAuth: []}],
+            tags: ['Types'],
             parameters: [
                 {
                     in: 'path',
                     name: 'id',
                     required: true,
-                    schema: {
-                        type: 'string',
-                        maxLength: 50
-                    },
-                    description: 'Id of nomenclator type to delete'
+                    schema: {type: 'integer'},
+                    description: 'User id of assignment'
                 }
             ],
             responses: {
                 200: {
-                    description: 'Success',
-                    content: {'application/json': { schema: { $ref: '#/components/schemas/Success'}}}
+                    description: 'ok',
+                    content: {'application/json': { schema: {$ref: '#/components/schemas/Success'}}}
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        },
+        get: {
+            security: [{bearerAuth: []}],
+            tags: ['Types'],
+            parameters: [
+                {
+                    in: 'path',
+                    name: 'id',
+                    required: true,
+                    schema: {type: 'integer'},
+                    description: 'User id of assignment'
+                }
+            ],
+            responses: {
+                200: {
+                    description: 'ok',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {
+                                    nomenclator: {
+                                        type: 'object',
+                                        properties: {
+                                            id: {type: 'string'},
+                                            description: {type: 'string'},
+                                            observation: {type: 'string'},
+                                            domain: {type: 'string'},
+                                            approved: {type: 'boolean'},
+                                            createdAt: {type: 'string'},
+                                            userCreator: {type: 'string'},
+                                            userDeleted: {type: 'string'},
+                                            deletedAt: {type: 'string'}
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
                 },
                 default: {
                     description: 'Error',
