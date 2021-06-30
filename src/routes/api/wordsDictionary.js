eef95fb3702c022081dae548de56f607cec8421f
lautaro valle lopez <lautarovallelopez@gmail.com>
2021-06-29 23:02:58 -0300

    feat(wordDictionary): download csv file

--- src/routes/api/wordsDictionary.js
+++ src/routes/api/wordsDictionary.js
@@ -5,6 +5,7 @@ module.exports = router => {
     router.route('/')
         .get(WordsDictionaryController.fetch)
         .post(WordsDictionaryController.create);
+    router.route('/downloadCsv').get(WordsDictionaryController.downloadCsv);
     router.get('/match/:word', WordsDictionaryController.findMatch);
     router.route('/:word')
         .get(WordsDictionaryController.find)
