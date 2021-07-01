a78c38ce6327d706cbca0ad56825f54ccb6a8ec9
lautaro valle lopez <lautarovallelopez@gmail.com>
2021-07-01 09:21:21 -0300

    feat(assigmentRole): download csv file

--- src/openapi/api/assignmentRoles.js
+++ src/openapi/api/assignmentRoles.js
@@ -262,5 +262,41 @@ module.exports = {
                 }
             }
         }
+    },
+    '/api/assignmentRoles/downloadCsv': {
+        get: {
+            security: [{bearerAuth: []}],
+            tags: ['Assignment Roles'],
+            parameters: [
+                {
+                    in: 'query',
+                    name: 'search',
+                    required: false,
+                    schema: {type: 'string'}
+                }
+            ],
+            responses: {
+                200: {
+                    description: 'Success',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                properties: {
+                                    dictionaryLinguistics: {
+                                        type: 'array',
+                                        items: {$ref: '#/components/schemas/DictionaryLinguistic'}
+                                    }
+                                }
+                            }
+                        }
+                    }
+                },
+                default: {
+                    description: 'Error',
+                    content: {'application/json': {schema: {$ref: '#/components/schemas/Error'}}}
+                }
+            }
+        }
     }
 };
