2f9e5f94db1ca636e4dd388d528e2a6ad3012dc5
Gustavo Albornoz <galbornoz@indec.gob.ar>
2021-07-28 11:21:53 -0300

    feat(): add files to new endpoint

---
+++ src/controllers/stepsLinguisticProcesses.js
@@ -0,0 +1,73 @@
+const { StepsEncodingProcessesService } = include('services');
+class StepsEncodingProcessesController {
+    static async fetch(req, res, next) {
+        try {
+            const encodingProcesses = await StepsEncodingProcessesService.fetch();
+            res.send({ encodingProcesses });
+        } catch(error) {
+            next(error);
+        }
+    }
+
+    static async find(req, res, next){
+        try {
+            const stepEncodingProcess = await StepsEncodingProcessesService.findOne(req.params);
+            res.send({stepEncodingProcess});
+        } catch(err) {
+            next(err);
+        }
+    }
+
+    static async create(req, res, next) {
+        try{
+            const stepEncodingProcess = await StepsEncodingProcessesService.create(req.body, req.user.id);
+            res.status(201);
+            res.send({ stepEncodingProcess });
+        } catch(err) {
+            next(err);
+        }
+    }
+
+    static async update(req, res, next){
+        try{
+            const stepEncodingProcess = await StepsEncodingProcessesService.update(req.params, req.body);
+            res.send({ success: true, stepEncodingProcess });
+        } catch(error) {
+            next(error);
+        }
+    }
+
+    static async delete(req, res, next){
+        try{
+            const result = await StepsEncodingProcessesService.delete(req.params, req.user.id);
+            if(result){
+                res.sendStatus(204);
+            }else{
+                res.sendStatus(400);
+            }
+        } catch(error) {
+            next(error);
+        }
+    }
+
+    static async fetchOne(req, res, next){
+        try{
+            const stepEncodingProcess = await StepsEncodingProcessesService.findById(req.params);
+            res.send({ stepEncodingProcess });
+        } catch(error) {
+            next(error);
+        }
+    }
+
+    static async downloadCsv(req, res, next){
+        try {
+            const stream = await StepsEncodingProcessesService.getCsv();
+            const buf = Buffer.from(stream, 'utf-8');
+            res.send(buf);
+        } catch(err) {
+            next(err);
+        }
+    }
+}
+
+module.exports = StepsEncodingProcessesController;
---
+++ src/services/stepsLinguisticProcesses.js
@@ -0,0 +1,163 @@
+const { stepsEncodingProcesses } = include('models');
+const OperativeSourcesService = require('./operativeSources');
+const QuestionsService = require('./questions');
+const EncodingProcessService = require('./encodingProcesses');
+const { dateToString, arrayToCsvFormat } = include('util');
+const map = require('lodash/map');
+const toNumber = require('lodash/toNumber');
+
+class StepsEncodingProcessesService {
+    static async fetch() {
+        let operatives = await stepsEncodingProcesses.find({FECHA_BAJA: null});
+        operatives = operatives.map(operative => ({
+            sourceId: operative.ID_FUENTE,
+            questionId: operative.ID_PREGUNTA,
+            order: operative.ORDEN,
+            encodingProcessId: operative.ID_PROCESO_CODIFICACION,
+            observation: operative.OBSERVACION,
+            domain: operative.DOMINIO,
+            userCreator: operative.ID_USUARIO_ALTA,
+            createdAt: dateToString(operative.FECHA_ALTA),
+            userDeleted: operative.ID_USUARIO_BAJA,
+            deletedAt: dateToString(operative.FECHA_BAJA)
+        }));
+        await OperativeSourcesService.getSourceData(operatives);
+        await QuestionsService.getQuestionData(operatives);
+        await EncodingProcessService.getEncodingProcessesData(operatives);
+        return operatives;
+    }
+
+    static async create(params, userCreator) {
+        const formattedOperativeSource = {
+            ID_FUENTE: params.sourceId,
+            ID_PREGUNTA: params.questionId,
+            ID_PROCESO_CODIFICACION: params.encodingProcessId,
+            ORDEN: params.order,
+            OBSERVACION: params.observation,
+            DOMINIO: params.domain,
+            ID_USUARIO_ALTA: userCreator,
+            FECHA_ALTA: new Date(),
+            ID_USUARIO_BAJA: null,
+            FECHA_BAJA: null
+        };
+        const operativeId = await stepsEncodingProcesses.insertOne(formattedOperativeSource, ['ID_FUENTE', 'ID_PREGUNTA', 'ORDEN', 'ID_PROCESO_CODIFICACION']);
+        console.log(operativeId);
+        const operative = await StepsEncodingProcessesService.findOne(
+            {sourceId: operativeId.ID_FUENTE,
+                questionId: operativeId.ID_PREGUNTA,
+                order: operativeId.ORDEN,
+                encodingProcessId: operativeId.ID_PROCESO_CODIFICACION});
+        return operative;
+    }
+
+    static async findOne(filters){
+        const formattedFilters = {
+            ID_FUENTE: filters.sourceId,
+            ID_PREGUNTA: filters.questionId,
+            ORDEN: filters.order,
+            ID_PROCESO_CODIFICACION: filters.encodingProcessId
+        };
+        const encodingProcess = await stepsEncodingProcesses.findById(formattedFilters);
+        return {
+            sourceId: encodingProcess.ID_FUENTE,
+            questionId: encodingProcess.ID_PREGUNTA,
+            order: encodingProcess.ORDEN,
+            encodingProcessId: encodingProcess.ID_PROCESO_CODIFICACION,
+            observation: encodingProcess.OBSERVACION,
+            domain: encodingProcess.DOMINIO,
+            userCreator: encodingProcess.ID_USUARIO_ALTA,
+            createdAt: dateToString(encodingProcess.FECHA_ALTA),
+            userDeleted: encodingProcess.ID_USUARIO_BAJA,
+            deletedAt: dateToString(encodingProcess.FECHA_BAJA)
+        };
+    }
+
+    static async update(filters, params){
+        const formattedOperativeSource = {
+            ID_FUENTE: toNumber(params.sourceId),
+            ID_PREGUNTA: toNumber(params.questionId),
+            ID_PROCESO_CODIFICACION: params.encodingProcessId,
+            ORDEN: toNumber(params.order),
+            OBSERVACION: params.observation,
+            DOMINIO: params.domain,
+            ID_USUARIO_ALTA: params.userCreator
+        };
+        const formattedFilters = {
+            ID_FUENTE: toNumber(filters.sourceId),
+            ID_PREGUNTA: toNumber(filters.questionId),
+            ORDEN: toNumber(filters.order),
+            ID_PROCESO_CODIFICACION: filters.encodingProcessId
+        };
+        const operativeId = await stepsEncodingProcesses.updateOne(formattedFilters, formattedOperativeSource, ['ID_FUENTE', 'ID_PREGUNTA', 'ORDEN', 'ID_PROCESO_CODIFICACION']);
+        const operative = await StepsEncodingProcessesService.findOne({sourceId: operativeId.ID_FUENTE,
+            questionId: operativeId.ID_PREGUNTA,
+            order: operativeId.ORDEN,
+            encodingProcessId: operativeId.ID_PROCESO_CODIFICACION});
+        return operative;
+    }
+
+    static async delete(filters, userDeleted){
+        const success = await stepsEncodingProcesses.deleteOne({
+            ID_FUENTE: filters.sourceId,
+            ID_PREGUNTA: filters.questionId,
+            ORDEN: filters.order,
+            ID_PROCESO_CODIFICACION: filters.encodingProcessId
+        },
+        {
+            FECHA_BAJA: new Date(),
+            ID_USUARIO_BAJA: userDeleted
+        });
+        return !!success;
+    }
+
+    static getCsv(){
+        return new Promise((resolve, reject) => {
+            let csvString = '';
+            const fieldNames = [
+                {
+                    nameInTable: 'ID_FUENTE',
+                    nameInFile: 'FUENTE'
+                },
+                {
+                    nameInTable: 'ID_PREGUNTA',
+                    nameInFile: 'PREGUNTA'
+                },
+                {
+                    nameInTable: 'ORDEN',
+                    nameInFile: 'ORDEN'
+                },
+                {
+                    nameInTable: 'ID_PROCESO_CODIFICACION',
+                    nameInFile: 'PROCESO DE CODIFICACION'
+                },
+                {
+                    nameInTable: 'OBSERVACION',
+                    nameInFile: 'OBSERVACIÓN'
+                },
+                {
+                    nameInTable: 'DOMINIO',
+                    nameInFile: 'DOMINIO'
+                }
+            ];
+
+            const operativeSourcesTableHeaders = map(fieldNames, field => field.nameInTable);
+            const operativeSourcesFileHeaders = map(fieldNames, field => field.nameInFile);
+            const headers = arrayToCsvFormat(operativeSourcesFileHeaders);
+            csvString += headers;
+            const stream = stepsEncodingProcesses.knex.select(operativeSourcesTableHeaders)
+                .from(stepsEncodingProcesses.tableName)
+                .stream();
+            stream.on('error', function(err) {
+                reject(err);
+            });
+            stream.on('data', function(data) {
+                csvString += arrayToCsvFormat(data);
+            });
+            stream.on('end', function() {
+                resolve(csvString);
+            });
+        });
+    }
+}
+
+module.exports = StepsEncodingProcessesService;
