bb170ea7bf57ba404a70e9bac33d2ef13460c3e1
Gustavo Albornoz <albornozgusti@gmail.com>
2021-08-19 12:02:50 -0300

    feat(): schema ready

--- src/openapi/components/schemas/microprocessesStepsOptions.js
+++ src/openapi/components/schemas/microprocessesStepsOptions.js
@@ -1,17 +1,14 @@
 module.exports = {
     type: 'object',
     properties: {
+        microprocessId: {type: 'string'},
+        orderId: {type: 'string'},
         sourceId: {type: 'string'},
         questionId: {type: 'string'},
-        dictionaryTypologyId: {type: 'string'},
         order: {type: 'string'},
-        linguisticFieldNameId: {type: 'string'},
-        showOnScreen: {type: 'string'},
+        abbreviation: {type: 'string'},
         observation: {type: 'string'},
-        domain: {type: 'string'},
         userCreator: {type: 'string'},
-        createdAt: {type: 'string'},
-        userDeleted: {type: 'string'},
-        deletedAt: {type: 'string'}
+        createdAt: {type: 'string'}
     }
 };
