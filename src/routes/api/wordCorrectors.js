04df5b93047230d221a439b2e38d3f185170e7c9
lautaro valle lopez <lautarovallelopez@gmail.com>
2021-11-01 12:34:42 -0300

    fix(newWords): correct words validation when create

--- src/routes/api/wordCorrectors.js
+++ src/routes/api/wordCorrectors.js
@@ -1,11 +1,10 @@
 const {WordCorrectorController} = include('controllers');
-const {validateWords} = include('routes/middlewares');
 
 module.exports = router => {
     router.route('/')
         .get(WordCorrectorController.fetch)
         .post(WordCorrectorController.create)
-        .put(validateWords, WordCorrectorController.update)
+        .put(WordCorrectorController.update)
         .delete(WordCorrectorController.delete);
     router.route('/downloadCsv').get(WordCorrectorController.downloadCsv);
     router.route('/:wrong/:right').get(WordCorrectorController.find);
