11b0388f0e2e6793cc382e5d0c0876119c96a7ce
galbornoz <galbornoz@indec.gob.ar>
2022-03-17 10:55:08 -0300

    feat(): modify model of relationshipBetweenNomenclators

--- src/models/relationshipBetweenNomenclators.js
+++ src/models/relationshipBetweenNomenclators.js
@@ -1,23 +1,16 @@
 const ModelCreate = include('helpers/modelCreate');
-const {wordsDictionaryTableName, wordsDictionaryAttrib} = include('constants');
+const {relationshipBetweenNomenclatorsTableName, relationshipBetweenNomenclatorsAttrib} = include('constants');
 const name = 'wordsDictionary';
 
-class WordsDictionary extends ModelCreate{
+class RelationshipBetweenNomenclators extends ModelCreate{
     constructor(props){
         super({
             ...props,
             name,
-            tableName: wordsDictionaryTableName,
-            selectableProps: wordsDictionaryAttrib
+            tableName: relationshipBetweenNomenclatorsTableName,
+            selectableProps: relationshipBetweenNomenclatorsAttrib
         });
-    }
-    async findWords(words) {
-        return await this.knex.select(['PALABRA'])
-            .from(this.tableName)
-            .whereIn('PALABRA', words)
-            .pluck('PALABRA')
-            .timeout(this.timeout);
     }
 }
 
-module.exports = knex => new WordsDictionary({knex});
+module.exports = knex => new RelationshipBetweenNomenclators({knex});
