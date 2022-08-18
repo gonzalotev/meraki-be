aa8436962d7023063d55da0f4435269a780bda0b
gtevez <gtevez@indec.gob.ar>
2022-08-17 23:23:50 -0300

    change port

--- src/services/protocol.js
+++ src/services/protocol.js
@@ -3,6 +3,7 @@ const knex = include('helpers/database');
 class ProtocolService {
     static fetch() {
         const protocols = knex.select('*').from('Protocolo');
+        console.log('asd');
         return protocols;
     }
 }
