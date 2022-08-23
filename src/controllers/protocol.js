47488ff6c981c548204466efd5b589bf069487bb
gtevez <gtevez@indec.gob.ar>
2022-08-23 16:47:29 -0300

    feat(): add services protocol

--- src/controllers/protocol.js
+++ src/controllers/protocol.js
@@ -3,10 +3,52 @@ const {ProtocolService} = include('services');
 class ProtocolController {
     static async fetch(req, res, next) {
         try {
-            const protocol = await ProtocolService.fetch();
+            const protocols = await ProtocolService.fetch();
+            console.log(protocols);
+            res.send(protocols);
+        } catch (error) {
+            next(error);
+        }
+    }
+    static async find(req, res, next) {
+        try {
+            const protocol = await ProtocolService.findOne(req.params);
             console.log(protocol);
             res.send(protocol);
         } catch (error) {
+            next(error);
+        }
+    }
+
+    static async delete(req, res, next){
+        try{
+            const result = await ProtocolService.deleteOne(req.params.serialKey);
+            if(result){
+                res.sendStatus(200);
+            }else{
+                res.sendStatus(400);
+            }
+        }catch(error){
+            next(error);
+        }
+    }
+
+    static async create(req, res, next){
+        try{
+            const protocol = await ProtocolService.create(req.body);
+            res.sendStatus(200);
+            res.send(protocol);
+        }catch(error){
+            next(error);
+        }
+    }
+
+    static async update(req, res, next){
+        try{
+            const protocol = await ProtocolService.update(req.body, req.params);
+            res.sendStatus(200);
+            res.send({protocol});
+        }catch(error){
             next(error);
         }
     }
--- src/services/protocol.js
+++ src/services/protocol.js
@@ -6,6 +6,40 @@ class ProtocolService {
         console.log('asd');
         return protocols;
     }
+
+    static findOne(filters){
+        const protocol = knex.select('*')
+            .from('Protocolo')
+            .where({Serialkey: filters.serialKey});
+        return protocol;
+    }
+    static deleteOne(serialKey){
+        return knex.from('Protocolo')
+            .where({Serialkey: serialKey})
+            .del()
+            .timeout(this.timeout);
+    }
+
+    static create(params){
+        const protocol = knex.insert({
+            Nombre: params.name,
+            Detalle: params.details
+        })
+            .into('Protocolo')
+            .timeout(this.timeout);
+        return protocol;
+    }
+
+    static update(params, serialKey){
+        const protocol = knex('Protocolo')
+            .update({
+                Nombre: params.name,
+                Detalle: params.details
+            })
+            .where(serialKey)
+            .timeout(this.timeout);
+        return protocol;
+    }
 }
 
 module.exports = ProtocolService;
